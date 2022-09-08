2f3b3a0b57248804f5e2f05de8ddb8bbaf1d51c8
Andres Saa <andressaa.dev@gmail.com>
2022-09-08 11:06:36 -0500

    Watt shop base structure

---
+++ src/main/java/com/wattshop/WattShopApplication.java
@@ -0,0 +1,13 @@
+package com.wattshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wattshop/WattShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wattshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
