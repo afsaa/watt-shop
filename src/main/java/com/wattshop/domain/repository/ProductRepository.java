d12be7dcbaf9c1dbc897f24b628b0e33fb6944b1
Andres Saa <andressaa.dev@gmail.com>
2022-09-09 15:05:04 -0500

    Use map struct to traduce persistence repositories to domain repositories

---
+++ src/main/java/com/wattshop/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.wattshop.domain.repository;
+
+import com.wattshop.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    List<Product> getByCategory(int categoryId);
+    Optional<List<Product>> getScarceProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
